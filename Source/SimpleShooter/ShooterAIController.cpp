ca766273165a568a97ddeb32d680cea53462420c
kleimaj <jacobakleiman@gmail.com>
2021-02-21 14:39:44 -0800

    adds BTServices to replace AIController Tick functionality

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -23,18 +23,4 @@ void AShooterAIController::BeginPlay()
 void AShooterAIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-
-	// If line of sight, move to actor and set focus
-	// Else, stop moving, clear focus
-	if (LineOfSightTo(PlayerPawn)) {
-		// Set PlayerLocation
-		GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"), PlayerPawn->GetActorLocation());
-		// setting LastKnown
-		GetBlackboardComponent()->SetValueAsVector(TEXT("LastKnownPlayerLocation"), PlayerPawn->GetActorLocation());
-	}
-	else {
-		// clear PlayerLocation
-		GetBlackboardComponent()->ClearValue(TEXT("PlayerLocation"));
-	}
 }
